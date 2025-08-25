156b9c0acf5a9e870a6f627c9cc92f2d1109f45b
Peter Kamau <kamaupeter343@gmail.com>
2025-08-25 16:40:08 +0300

    Introduced an authentication context to the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import { useAuth } from "@/components/leofresh/auth";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { SidebarProvider } from "./components/ui/sidebar";
 import "./index.css";
 import { routeTree } from "./routeTree.gen";
 import { store } from "./store";
@@ -18,12 +19,23 @@ declare module "@tanstack/react-router" {
 	}
 }
 
+function AuthenticatedApp() {
+	const auth = useAuth();
+
+	return (
+		<SidebarProvider>
+			<RouterProvider
+				router={router}
+				context={auth}
+			/>
+		</SidebarProvider>
+	);
+}
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Provider store={store}>
-			<SidebarProvider>
-				<RouterProvider router={router} />
-			</SidebarProvider>
+			<AuthenticatedApp />
 		</Provider>
 	</StrictMode>
 );
