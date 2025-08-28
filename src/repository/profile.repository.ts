b5071a544e725b163d90c378f8505c372870d248
Peter Kamau <kamaupeter343@gmail.com>
2025-08-28 20:22:25 +0300

    Added the Profile Repository

---
+++ src/repository/profile.repository.ts
@@ -0,0 +1,14 @@
+import { POSProfile } from "@/data-access/api/profile";
+import type { ReadProfileDTO } from "@/data-access/models/profile.dto";
+
+export interface ProfileRepository {
+	retrievePOSProfile(email?: string): Promise<ReadProfileDTO[]>;
+}
+
+export class UserProfileRepository implements ProfileRepository {
+	constructor() {}
+	async retrievePOSProfile(email?: string): Promise<ReadProfileDTO[]> {
+		const profile = new POSProfile({ docType: "POS Profile" });
+		return profile.retrievePOSProfile({ email });
+	}
+}
