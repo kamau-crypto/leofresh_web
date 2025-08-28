84bc0b4505b8066429707342eb6d57dd1528aa20
Peter Kamau <kamaupeter343@gmail.com>
2025-08-28 20:19:32 +0300

    created the profile DTO

---
+++ src/data-access/models/profile.dto.ts
@@ -0,0 +1,20 @@
+export interface ReadProfileDTO {
+	customer: string;
+	company: string;
+	warehouse_name: string;
+	source_warehouse: string;
+	cost_center: string;
+	currency: string;
+	selling_price_list: string;
+	user_email: string;
+	project: string;
+	lnmo: string | null;
+	bank_no: string | null;
+	bank_account: string;
+	expense_account: string;
+	income_account: string;
+	debtor_account: string;
+	unrealized_profit: string;
+	waste_water: number;
+	write_off_account: string;
+}
