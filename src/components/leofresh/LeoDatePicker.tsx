07fc3d1a1db242c9e7228462c6c7d5f1a48c8e8d
Peter Kamau <kamaupeter343@gmail.com>
2025-08-29 19:46:24 +0300

    Added some popovers on top of the date picker component of the app

--- src/components/leofresh/LeoDatePicker.tsx
+++ src/components/leofresh/LeoDatePicker.tsx
@@ -1,30 +1,54 @@
+import { cn } from "@/lib/utils";
+import { CalendarIcon } from "lucide-react";
 import React, { useState } from "react";
+import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 export const LeoDatePicker = ({
 	value,
 	onChange,
+	label,
 }: {
-	value: string;
-	onChange: React.Dispatch<React.SetStateAction<string>>;
+	value: Date | null;
+	label: string;
+	onChange: React.Dispatch<React.SetStateAction<Date | null>>;
 }) => {
-	const [date, setDate] = useState<Date>(new Date(value) ?? new Date());
+	const [date, setDate] = useState<Date>(value ? new Date(value) : new Date());
 
 	const handleChange = (newDate: Date) => {
 		setDate(newDate);
-		onChange(newDate.toLocaleString());
+		onChange(newDate);
 	};
 
 	return (
-		<div className='relative'>
-			<Calendar
-				mode='single'
-				defaultMonth={date}
-				required={true}
-				selected={date}
-				onSelect={handleChange}
-				className='pr-10'
-			/>
-		</div>
+		<>
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button
+						variant={"outline"}
+						className={cn("w-[240px] pl-3 text-left font-normal")}>
+						<span>{label}</span>
+						<CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent
+					className='w-auto p-0'
+					align='start'>
+					<Calendar
+						mode='single'
+						selected={date}
+						required={true}
+						defaultMonth={date}
+						onSelect={handleChange}
+						disabled={date =>
+							date > new Date() || date < new Date("1900-01-01")
+						}
+						captionLayout='dropdown'
+						className='pr-10'
+					/>
+				</PopoverContent>
+			</Popover>
+		</>
 	);
 };
