ae32d12ae2b2b31e08fa1fc9ce20fa463f312065
Peter Kamau <kamaupeter343@gmail.com>
2025-08-29 10:22:14 +0300

    exported the expenses model

--- src/data-access/models/index.ts
+++ src/data-access/models/index.ts
@@ -1,3 +1,4 @@
 //Models are the responses of what you receive from the server, easier for data
 export type * from "./banking.model";
+export type * from "./expenses.model";
 export type * from "./payment.model";
