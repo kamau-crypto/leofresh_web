2b34b1330cbe9d70424ff109af3089bc6717f822
Peter Kamau <kamaupeter343@gmail.com>
2025-09-01 23:35:22 +0300

    fixed the positioning of the add item button

--- src/components/pages/items/Items.tsx
+++ src/components/pages/items/Items.tsx
@@ -12,18 +12,19 @@ export function ItemsPage({
 	return (
 		<div className='container mx-auto py-10 shadow-xl shadow-primary/10 rounded-xl p-3 overflow-y-hidden'>
 			{(title || description) && (
-				<div className='mb-6 max-h-[20%] flex w-full flex-col'>
+				<div className='mb-6 w-full flex flex-col'>
 					{title && (
 						<h1 className='text-2xl font-bold text-gray-900 mb-2'>{title}</h1>
 					)}
 					{description && <p className='text-gray-600'>{description}</p>}
-					<LeoButton
-						variant='default'
-						className='flex justify-end fit-content'>
-						Add Item <PlusCircle className='ml-2 w-4 h-4' />
-					</LeoButton>
 				</div>
 			)}
+			{/* Button aligned to the right */}
+			<div className='flex justify-end mb-4'>
+				<LeoButton variant='default'>
+					Add Item <PlusCircle className='ml-2 w-4 h-4' />
+				</LeoButton>
+			</div>
 			<div className='max-h-[80%] overflow-y-auto rounded-md'>
 				<ItemList />
 			</div>
