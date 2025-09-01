c70bec526fa54e021f5ea953fc7a9245ee5f7c74
Peter Kamau <kamaupeter343@gmail.com>
2025-09-01 13:56:23 +0300

    Added the items datatable

---
+++ src/components/pages/items/list/itemList.tsx
@@ -0,0 +1,25 @@
+import { LeofreshDataTable } from "@/components/leofresh/";
+import { useListAllProducts } from "@/hooks/item";
+import { useMemo } from "react";
+import { itemColumns } from "./columns";
+
+export function ItemList() {
+	const { data, isLoading } = useListAllProducts();
+
+	const memoizedData = useMemo(() => {
+		if (!data) return [];
+		return data;
+	}, [data]);
+
+	console.log("Memoized Data", memoizedData);
+
+	return (
+		<LeofreshDataTable
+			data={memoizedData}
+			isLoading={isLoading}
+			columns={itemColumns}
+			filterPlaceHolder='Search items by Name'
+			primaryFilter='item_name'
+		/>
+	);
+}
