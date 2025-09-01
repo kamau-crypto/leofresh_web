0d8f916e80cb569af1dc8b8cf1fa293d30cb9310
Peter Kamau <kamaupeter343@gmail.com>
2025-09-01 13:56:09 +0300

    added the items pages component

---
+++ src/components/pages/items/Items.tsx
@@ -0,0 +1,32 @@
+import { LeoButton } from "@/components/leofresh/LeoButton.tsx";
+import { PlusCircle } from "lucide-react";
+import { ItemList } from "./list/itemList.tsx";
+
+export function ItemsPage({
+	title,
+	description,
+}: {
+	title?: string;
+	description?: string;
+}) {
+	return (
+		<div className='container mx-auto py-10 shadow-xl shadow-primary/10 rounded-xl p-3 overflow-y-hidden'>
+			{(title || description) && (
+				<div className='mb-6 max-h-[20%] flex w-full flex-col'>
+					{title && (
+						<h1 className='text-2xl font-bold text-gray-900 mb-2'>{title}</h1>
+					)}
+					{description && <p className='text-gray-600'>{description}</p>}
+					<LeoButton
+						variant='default'
+						className='flex justify-end fit-content'>
+						Add Item <PlusCircle className='ml-2 w-4 h-4' />
+					</LeoButton>
+				</div>
+			)}
+			<div className='max-h-[80%] overflow-y-auto rounded-md'>
+				<ItemList />
+			</div>
+		</div>
+	);
+}
