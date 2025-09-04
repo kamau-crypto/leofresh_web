5d412aa2485868b04518ff08bcea3012d9a5ab67
Peter Kamau <kamaupeter343@gmail.com>
2025-09-04 17:22:52 +0300

    corrected to avoid repetitions

--- src/components/pages/supplier/list/SuppliersList.tsx
+++ src/components/pages/supplier/list/SuppliersList.tsx
@@ -1,15 +1,17 @@
 import { LeofreshDataTable } from "@/components/leofresh";
+import type { SupplierFilterEntity } from "@/domain";
 import { useListSuppliers } from "@/hooks/supplier";
 import { useMemo, useState } from "react";
 import { supplierColumns } from "./columns";
 
 export function SuppliersList() {
-	const [order_by, setOrderBy] = useState<string>("modified desc");
-	const [limit_start, setLimitStart] = useState<number>(0);
-	const [limit_page_length, setLimitPageLength] = useState<number>(100);
-	const supplierParams = { order_by, limit_start, limit_page_length };
+	const [filter, _setFilter] = useState<Omit<SupplierFilterEntity, "fields">>({
+		limit_page_length: 100,
+		limit_start: 0,
+		order_by: "modified desc",
+	});
 
-	const { data, isLoading } = useListSuppliers({ supplierParams });
+	const { data, isLoading } = useListSuppliers({ supplierParams: filter });
 
 	const memoizedData = useMemo(() => {
 		if (!data) return [];
