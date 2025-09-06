28c2d6f1eed28b5e5fb66c511b32c9ee4caf498b
Peter Kamau <kamaupeter343@gmail.com>
2025-09-06 12:29:59 +0300

    simplified the user profile matching with the app

--- src/use-cases/profile.use-case.ts
+++ src/use-cases/profile.use-case.ts
@@ -1,3 +1,5 @@
+import { LeofreshError } from "@/lib/error";
+import { extractFrappeErrorMessage } from "@/lib/frappe_error_handler";
 import type { ProfileRepository } from "@/repository/profile.repository";
 
 export class profileUseCase {
@@ -6,8 +8,16 @@ export class profileUseCase {
 	constructor(profileRepository: ProfileRepository) {
 		this.profileRepository = profileRepository;
 	}
-	async execute(email?: string) {
-		const profile = await this.profileRepository.retrievePOSProfile(email);
-		return profile.filter(p => p.user_email === email);
+	async execute({ email, name }: { email?: string; name?: string }) {
+		try {
+			const profile = await this.profileRepository.retrievePOSProfile(email);
+			return profile.filter(
+				p =>
+					p.user_email.toLowerCase() === email ||
+					p.user_email.toLowerCase() === name
+			);
+		} catch (error) {
+			throw new LeofreshError({ message: extractFrappeErrorMessage(error) });
+		}
 	}
 }
