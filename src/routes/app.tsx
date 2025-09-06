69bdf0d608ee0fa47045f2b405721a65da0fca87
Peter Kamau <kamaupeter343@gmail.com>
2025-09-06 22:53:00 +0300

    Handled can go back navigation for the app

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,6 +1,13 @@
 "use client";
-import { LeoFreshSideBar } from "@/components";
-import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import { LeoFreshSideBar, NotFoundError } from "@/components";
+import { Button } from "@/components/ui/button";
+import {
+	createFileRoute,
+	Outlet,
+	redirect,
+	useCanGoBack,
+	useRouter,
+} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app")({
 	component: Index,
@@ -10,6 +17,32 @@ export const Route = createFileRoute("/app")({
 				to: "/auth/login",
 			});
 		}
+	},
+	notFoundComponent: () => {
+		const router = useRouter();
+		const canGoBack = useCanGoBack();
+
+		const handleBack = () => {
+			if (canGoBack) {
+				router.history.back();
+			} else {
+				router.navigate({ to: "/auth/login" });
+			}
+		};
+
+		return (
+			<div className='w-full h-full mx-auto flex flex-col justify-center items-center p-4'>
+				<NotFoundError />
+				<p className='mt-4 text-center text-lg font-inter'>
+					The requested resource was not found.
+				</p>
+				<Button
+					variant='link'
+					onClick={() => handleBack()}>
+					Go back
+				</Button>
+			</div>
+		);
 	},
 });
 
