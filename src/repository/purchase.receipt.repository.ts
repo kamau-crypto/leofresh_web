de006e587f5e092697c3c0442be4e5e78e568e1d
Peter Kamau <kamaupeter343@gmail.com>
2025-09-08 11:18:50 +0300

    retrieved purchase receipts used in the application

--- src/repository/purchase.receipt.repository.ts
+++ src/repository/purchase.receipt.repository.ts
@@ -1,15 +1,39 @@
-import { LeofreshError } from "@/lib/error";
+import { PurchaseReceiptDataSource } from "@/data-access/api/purchase-receipt";
+import type {
+	PurchaseReceiptsFilterEntity,
+	PurchaseReceiptsListEntity,
+} from "@/domain/entities/purchase/purchase.receipt.entity";
 
 export interface IPurchaseReceiptRepository {
-	getAllPurchaseReceipts(): Promise<any[]>;
+	getAllPurchaseReceipts({
+		filter,
+	}: {
+		filter: Omit<PurchaseReceiptsFilterEntity, "fields">;
+	}): Promise<PurchaseReceiptsListEntity[]>;
 }
 
 export class PurchaseReceiptRepository implements IPurchaseReceiptRepository {
 	constructor() {}
-	async getAllPurchaseReceipts(): Promise<any[]> {
-		throw new LeofreshError({
-			message: "Method not implemented. We cannot fetch all Purchase Receipts.",
+	async getAllPurchaseReceipts({
+		filter,
+	}: {
+		filter: Omit<PurchaseReceiptsFilterEntity, "fields">;
+	}): Promise<PurchaseReceiptsListEntity[]> {
+		const fields = [
+			"name",
+			"supplier",
+			"status",
+			"cost_center",
+			"docstatus",
+			"grand_total",
+			"creation",
+			"modified",
+			"owner",
+		];
+
+		const dataSource = new PurchaseReceiptDataSource({
+			docType: "Purchase Receipt",
 		});
-		return [];
+		return await dataSource.retrievePurchaseReceipts({ ...filter, fields });
 	}
 }
--- src/use-cases/purchase.receipt.use-case.ts
+++ src/use-cases/purchase.receipt.use-case.ts
@@ -1,6 +1,33 @@
+import type { PurchaseReceiptsFilterEntity } from "@/domain/entities/purchase/purchase.receipt.entity";
+import { LeofreshError } from "@/lib/error";
+import { extractFrappeErrorMessage } from "@/lib/frappe_error_handler";
+import type { PurchaseReceiptRepository } from "@/repository/purchase.receipt.repository";
+
 export class PurchaseReceiptUseCase {
-    //Get a list of all purchase receipts
-    getAllPurchaseReceipts() {
-        return [];
-    }
+	private pRRepository: PurchaseReceiptRepository;
+
+	constructor({ pRRepository }: { pRRepository: PurchaseReceiptRepository }) {
+		this.pRRepository = pRRepository;
+	}
+
+	//Get a list of all purchase receipts
+	allPurchaseReceipts(filter: Omit<PurchaseReceiptsFilterEntity, "fields">) {
+		try {
+			return this.pRRepository.getAllPurchaseReceipts({ filter });
+		} catch (error) {
+			throw new LeofreshError({
+				message: extractFrappeErrorMessage({ error }),
+			});
+		}
+	}
+	//Create Purchase Receipt
+	makePurchaseReceipt() {
+		throw new LeofreshError({ message: "Not Implemented" });
+	}
+
+	//
+	//Read Purchase Receipt
+	PurchaseReceiptDetails({}: { name: string }) {
+		throw new LeofreshError({ message: "Not Implemented" });
+	}
 }
