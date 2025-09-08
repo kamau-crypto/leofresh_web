650eef17e72d247205bbe32f67f000a5af7714cf
Peter Kamau <kamaupeter343@gmail.com>
2025-09-08 12:41:43 +0300

    Added the purchase Order Page

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,3 +2,5 @@ export * from "./customer/Customer";
 export * from "./items/Items";
 export * from "./purchase/invoice/PurchaseInvoice";
 export * from "./supplier/Suppliers";
+export * from "./purchase/receipt/PurchaseReceipt";
+export * from "./purchase/order/PurchaseOrder";
