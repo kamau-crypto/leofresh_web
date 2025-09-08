833557e1b0ee74b71caaa92b2944b77ae61cd8e9
Peter Kamau <kamaupeter343@gmail.com>
2025-09-08 16:28:44 +0300

    created a reusable Data Column Selection menu

---
+++ src/components/leofresh/LeofreshDataTable/DataColumnsDefinition.tsx
@@ -0,0 +1,27 @@
+import { Checkbox } from "@/components/ui/checkbox";
+import type { ColumnDef } from "@tanstack/react-table";
+
+export function createSelectionColumn<TData>(): ColumnDef<TData> {
+	return {
+		id: "select",
+		header: ({ table }) => (
+			<Checkbox
+				checked={
+					table.getIsAllPageRowsSelected() ||
+					(table.getIsSomePageRowsSelected() && "indeterminate")
+				}
+				onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+				aria-label='Select all'
+			/>
+		),
+		cell: ({ row }) => (
+			<Checkbox
+				checked={row.getIsSelected()}
+				onCheckedChange={value => row.toggleSelected(!!value)}
+				aria-label='Select row'
+			/>
+		),
+		enableSorting: false,
+		enableHiding: false,
+	};
+}
