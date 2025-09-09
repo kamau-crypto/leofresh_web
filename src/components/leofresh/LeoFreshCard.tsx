bf2edf47ee285451ba741b2bb4456bedcd4c6a25
Peter Kamau <kamaupeter343@gmail.com>
2025-09-09 14:54:23 +0300

    Added the leofresh Card component

---
+++ src/components/leofresh/LeoFreshCard.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "../ui/card";
+
+export type LeoFreshCardProps = {
+	title?: string;
+	description?: string;
+	footer?: string;
+	content: React.ReactNode;
+	className?: string;
+};
+
+export function LeoFreshCard({
+	title,
+	description,
+	footer,
+	content,
+	className,
+}: LeoFreshCardProps) {
+	<Card
+		className={cn(
+			"shadow-lg shadown-primary/10 rounded-md lg:rounded-lg",
+			className
+		)}>
+		<CardHeader>
+			<CardTitle>{title}</CardTitle>
+			<CardDescription>{description}</CardDescription>
+		</CardHeader>
+		<CardContent>{content}</CardContent>
+		<CardFooter>
+			<p>{footer}</p>
+		</CardFooter>
+	</Card>;
+}
