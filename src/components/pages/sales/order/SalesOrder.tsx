52ddc37e091c2ce09a5aaeafbc820a00bd319a81
Peter Kamau <kamaupeter343@gmail.com>
2025-09-10 23:27:46 +0300

    Created a reusable Sales Order component

--- src/components/pages/sales/order/SalesOrder.tsx
+++ src/components/pages/sales/order/SalesOrder.tsx
@@ -1,5 +1,11 @@
-import { LeoButton, LeoWrapper } from "@/components/leofresh";
+import {
+	LeoButton,
+	LeoFreshDialogButtons,
+	LeoFreshDialogForm,
+	LeoWrapper,
+} from "@/components/leofresh";
 import { PlusCircle } from "lucide-react";
+import { SalesOrderForm } from "./form/SalesOrderForm";
 import { SalesOrderList } from "./list/SalesOrderList";
 
 export function SalesOrder() {
@@ -7,9 +13,16 @@ export function SalesOrder() {
 		<LeoWrapper
 			Content={<SalesOrderList />}
 			HeaderButtons={
-				<LeoButton>
-					Add Sales Order <PlusCircle className='ml-2 w-4 h-4' />
-				</LeoButton>
+				<LeoFreshDialogForm
+					description='Record Sales to your customers filling this form.'
+					title='New Sales Order'
+					DialogTriggerChild={
+						<LeoButton>
+							Add Sales Order <PlusCircle className='ml-2 w-4 h-4' />
+						</LeoButton>
+					}
+					DialogChild={<SalesOrderForm Buttons={<LeoFreshDialogButtons />} />}
+				/>
 			}
 			description='Manage Sales Orders created by clients here.'
 			title='Sales Order'
