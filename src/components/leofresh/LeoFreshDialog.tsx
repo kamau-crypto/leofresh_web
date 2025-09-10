01a2fb4575628d3bd42888bcfc2053fa848fb6d1
Peter Kamau <kamaupeter343@gmail.com>
2025-09-10 23:28:37 +0300

    Created a form compatible dialog component

--- src/components/leofresh/LeoFreshDialog.tsx
+++ src/components/leofresh/LeoFreshDialog.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
 	Dialog,
 	DialogClose,
@@ -10,26 +9,66 @@ import {
 	DialogTrigger,
 } from "@/components/ui/dialog";
 import type React from "react";
+import { Button } from "../ui/button";
+import { LeoButton } from "./LeoButton";
+
+export type LeoFreshDialogFormProps = {
+	DialogChild: React.ReactNode;
+	DialogTriggerChild: React.ReactNode;
+	title: string;
+	description: string;
+};
+
+export function LeoFreshDialogForm({
+	DialogChild,
+	DialogTriggerChild,
+	title,
+	description,
+}: LeoFreshDialogFormProps) {
+	return (
+		<Dialog>
+			<DialogTrigger asChild>{DialogTriggerChild}</DialogTrigger>
+			<DialogContent className='md:max-w-4xl overflow-y-scroll max-h-[80vh] absolute'>
+				<DialogHeader className='sticky'>
+					<DialogTitle>{title}</DialogTitle>
+					<DialogDescription>{description}</DialogDescription>
+				</DialogHeader>
+				{DialogChild}
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+export function LeoFreshDialogButtons() {
+	return (
+		<DialogFooter className='grid grid-cols-2 gap-4'>
+			<DialogClose asChild>
+				<Button variant='outline'>Cancel</Button>
+			</DialogClose>
+			<LeoButton type='submit'>Create</LeoButton>
+		</DialogFooter>
+	);
+}
 
 export type LeoFreshDialogProps = {
 	DialogChild: React.ReactNode;
+	DialogTriggerChild: React.ReactNode;
 	DialogSubmitButton?: React.ReactNode;
 	title: string;
 	description: string;
-	DialogTriggerChild: React.ReactNode;
 };
 
 export function LeoFreshDialog({
+	DialogChild,
+	DialogTriggerChild,
+	DialogSubmitButton,
 	title,
 	description,
-	DialogChild,
-	DialogSubmitButton,
-	DialogTriggerChild,
 }: LeoFreshDialogProps) {
 	return (
 		<Dialog>
 			<DialogTrigger asChild>{DialogTriggerChild}</DialogTrigger>
-			<DialogContent className='sm:max-w-[425px]'>
+			<DialogContent className='sm:max-w-[425px] md:max-w-2xl overflow-y-scroll max-h=[80vh]'>
 				<DialogHeader>
 					<DialogTitle>{title}</DialogTitle>
 					<DialogDescription>{description}</DialogDescription>
