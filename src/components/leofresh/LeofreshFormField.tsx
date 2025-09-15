cf2d6d8cae961a0d924c1ec7e831f0c5e3fe3fae
Peter Kamau <kamaupeter343@gmail.com>
2025-09-15 12:10:01 +0300

    Made the form field reusable

--- src/components/leofresh/LeofreshFormField.tsx
+++ src/components/leofresh/LeofreshFormField.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { cn } from "@/lib/utils";
 import type {
 	Control,
 	FieldError,
@@ -15,26 +17,10 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 
-// export interface HillFreshFeldFormProps<T extends FieldValues> {
-// 	labelText?: string;
-// 	control: Control<T, any>;
-// 	name: Path<T>;
-// 	error: FieldError | undefined;
-// 	valueAsNumber?: boolean;
-// 	style?: StyleProp<TextStyle>;
-// 	right?: string;
-// 	left?: string;
-// 	dense?: boolean;
-// 	keyBoardType?: KeyboardTypeOptions;
-// 	multiline?: boolean;
-// 	defaultValue?: PathValue<T, Path<T>>;
-// 	disabled?: boolean;
-// }
-
 export interface LeoFreshFieldForm<T extends FieldValues> {
 	control: Control<T, any>;
 	name: Path<T>;
-	error: FieldError | undefined;
+	error?: FieldError;
 
 	className?: string;
 	right?: React.ReactNode;
@@ -54,25 +40,50 @@ export function LeofreshFormField<T extends FieldValues>({
 	left,
 	right,
 	labelText,
-	type,
+	type = "text",
 	description,
+	disabled,
+	className,
 }: LeoFreshFieldForm<T>) {
 	return (
 		<FormField
 			control={control}
 			name={name}
 			render={({ field }) => (
-				<FormItem className='grid gap-3'>
+				<FormItem className={cn("grid gap-3", className)}>
 					{labelText && <FormLabel>{labelText}</FormLabel>}
-					<FormControl>
-						<Input
-							left={left}
-							type={type}
-							right={right}
-							placeholder={placeholder ?? ""}
-							{...field}
-						/>
-					</FormControl>
+					<div className='relative'>
+						{left && (
+							<div className='absolute left-3 top-1/2 -translate-y-1/2 z-10'>
+								{left}
+							</div>
+						)}
+						<FormControl>
+							<Input
+								type={type}
+								placeholder={placeholder ?? ""}
+								disabled={disabled}
+								className={cn(left && "pl-10", right && "pr-10")}
+								{...field}
+								value={field.value ?? ""}
+								onChange={e => {
+									// Handle number conversion for number inputs
+									if (type === "number") {
+										const value =
+											e.target.value === "" ? "" : Number(e.target.value);
+										field.onChange(value);
+									} else {
+										field.onChange(e.target.value);
+									}
+								}}
+							/>
+						</FormControl>
+						{right && (
+							<div className='absolute right-3 top-1/2 -translate-y-1/2 z-10'>
+								{right}
+							</div>
+						)}
+					</div>
 					{description && <FormDescription>{description}</FormDescription>}
 					<FormMessage />
 				</FormItem>
