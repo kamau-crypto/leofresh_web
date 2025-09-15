2b58ea9ee82497a3ed7a44b4bb8ad8cdb357bbf6
Peter Kamau <kamaupeter343@gmail.com>
2025-09-15 10:51:48 +0300

    Added the items repository

--- src/repository/item.repository.ts
+++ src/repository/item.repository.ts
@@ -1,33 +1,29 @@
-import type { GetItemsDTO } from "@/data-access/dto";
+import { ItemsForSaleDataSource } from "@/data-access/api/item";
+import type { ItemListEntity } from "@/domain";
 
-interface IItemRepository {
-	getFinishedProductsToSellOrBuy(fields: GetItemsDTO): Promise<void>;
-	getNamingSeries(): Promise<{ naming_series: string }[]>;
+export interface IItemRepository {
+	getAllItems(): Promise<ItemListEntity[]>;
 }
 
-/**
- * 
- * 
- * export enum ItemFieldsEnum {
-	item_name = "item_name",
-	item_code = "item_code",
-	item_group = "item_group",
-	standard_buying_uom = "standard_buying_uom",
-	standard_selling_uom = "standard_selling_uom",
-	image = "image",
-}
-
-
- */
-
-export class ItemRepository {
-	constructor() {}
-
-	getFinishedProductsToSellOrBuy(fields: GetItemsDTO): Promise<void> {
-		// Implementation goes here
+export class ItemRepository implements IItemRepository {
+	private ItemDataSource: ItemsForSaleDataSource;
+	constructor() {
+		this.ItemDataSource = new ItemsForSaleDataSource({ docType: "Item" });
 	}
 
-	getNamingSeries(): Promise<void> {
-		// Implementation goes here
+	async getAllItems(): Promise<ItemListEntity[]> {
+		const data = await this.ItemDataSource.getAllItems();
+		return this.mapItemsToDomain({ items: data.data.data });
+	}
+
+	private mapItemsToDomain({
+		items,
+	}: {
+		items: Pick<ItemListEntity, "name">[];
+	}): ItemListEntity[] {
+		return items.map(item => ({
+			name: item.name,
+			value: item.name,
+		}));
 	}
 }
