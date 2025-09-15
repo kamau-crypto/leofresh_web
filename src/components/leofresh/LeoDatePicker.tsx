e533d9fabc515a0583652424d2da6b6e6751d425
Peter Kamau <kamaupeter343@gmail.com>
2025-09-15 12:09:22 +0300

    Made the date picker component reusable within the form

--- src/components/leofresh/LeoDatePicker.tsx
+++ src/components/leofresh/LeoDatePicker.tsx
@@ -5,50 +5,54 @@ import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
+interface LeoFreshDatePickerProps {
+	value: Date | null;
+	label: string;
+	className?: string;
+	disabled?: boolean;
+	onChange: React.Dispatch<React.SetStateAction<Date | null>>;
+}
+
 export const LeoDatePicker = ({
 	value,
 	onChange,
 	label,
-}: {
-	value: Date | null;
-	label: string;
-	onChange: React.Dispatch<React.SetStateAction<Date | null>>;
-}) => {
+	className,
+	disabled,
+}: LeoFreshDatePickerProps) => {
 	const [date, setDate] = useState<Date>(value ? new Date(value) : new Date());
 
 	const handleChange = (newDate: Date) => {
 		setDate(newDate);
 		onChange(newDate);
 	};
+	const isDisabled =
+		disabled ?? (() => date > new Date() || date < new Date("1900-01-01"));
 
 	return (
-		<>
-			<Popover>
-				<PopoverTrigger asChild>
-					<Button
-						variant={"outline"}
-						className={cn("w-[240px] pl-3 text-left font-normal")}>
-						<span>{label}</span>
-						<CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
-					</Button>
-				</PopoverTrigger>
-				<PopoverContent
-					className='w-auto p-0'
-					align='start'>
-					<Calendar
-						mode='single'
-						selected={date}
-						required={true}
-						defaultMonth={date}
-						onSelect={handleChange}
-						disabled={date =>
-							date > new Date() || date < new Date("1900-01-01")
-						}
-						captionLayout='dropdown'
-						className='pr-10'
-					/>
-				</PopoverContent>
-			</Popover>
-		</>
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button
+					variant={"outline"}
+					className={cn("w-[240px] pl-3 text-left font-normal", className)}>
+					<span>{label}</span>
+					<CalendarIcon className='ml-auto size-4 opacity-50' />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent
+				className='w-auto p-0'
+				align='start'>
+				<Calendar
+					mode='single'
+					selected={date}
+					required={true}
+					defaultMonth={date}
+					onSelect={handleChange}
+					disabled={isDisabled}
+					captionLayout='dropdown'
+					className='pr-10'
+				/>
+			</PopoverContent>
+		</Popover>
 	);
 };
