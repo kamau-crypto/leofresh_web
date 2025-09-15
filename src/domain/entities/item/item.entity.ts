681bcdad8f697b72072d800683265c45a0ff13f5
Peter Kamau <kamaupeter343@gmail.com>
2025-09-15 22:45:32 +0300

    Added the items general entity

---
+++ src/domain/entities/item/item.entity.ts
@@ -0,0 +1,31 @@
+import type { CommonEntity } from "../common/common.entities";
+import type { PurchaseItemEntity } from "./item-price.entity";
+
+export interface ItemListFieldsEntity
+	extends Pick<CommonEntity, "fields" | "limit_page_length"> {}
+
+export interface AssociatedItemsEntity extends PurchaseItemEntity {
+	associated_items: ListItemWithValueEntity[];
+}
+
+export interface CommonItemEntity {
+	item_name: string;
+	item_code: string;
+	item_group: string;
+	stock_uom: string;
+	uoms: string;
+	image: string;
+}
+
+export interface ManufactuatringItemListEntity extends CommonItemEntity {
+	conversion_factor: number;
+	purchase_uom: string;
+	uom: string;
+}
+
+export interface ListItemWithValueEntity extends ManufactuatringItemListEntity {
+	value: string;
+}
+
+export interface ManufacturingMaterialsFieldsEntity
+	extends Pick<CommonEntity, "limit_page_length" | "limit_start"> {}
