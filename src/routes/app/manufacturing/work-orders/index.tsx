fe760c13d80e63d8766ad85c10d4f9e2066a49f8
Peter Kamau <kamaupeter343@gmail.com>
2025-09-16 15:11:47 +0300

    Added the manufacturing main report and other components for reuse within the app

---
+++ src/routes/app/manufacturing/work-orders/index.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/app/manufacturing/work-orders/")({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	return <div>Hello "/app/manufacturing/work-orders/"!</div>;
+}
