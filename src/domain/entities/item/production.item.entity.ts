eff8aef2b236217faacee912249d160e02a42307
Peter Kamau <kamaupeter343@gmail.com>
2025-09-16 17:27:41 +0300

    Added a helper BOM

--- src/domain/entities/item/production.item.entity.ts
+++ src/domain/entities/item/production.item.entity.ts
@@ -3,7 +3,7 @@ import type { PurchaseItemEntity } from "./item-price.entity";
 export interface ListProductionItemEntity {
 	created_item: string;
 	item_name: string;
-	rate: number;
+	rate: number; // Provided later on during the manufacture process
 	qty: number;
 }
 
