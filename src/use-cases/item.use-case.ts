6ac0decf8020f3149c7440f9f07fad7fdb6f6799
Peter Kamau <kamaupeter343@gmail.com>
2025-09-16 14:59:31 +0300

    clean up and separation of concerns

--- src/use-cases/item.use-case.ts
+++ src/use-cases/item.use-case.ts
@@ -1,4 +1,4 @@
-import type { AssociatedItemsEntity, ItemListFieldsEntity } from "@/domain";
+import type { ManufacturingMaterialsFieldsEntity } from "@/domain";
 import { ItemRepository } from "@/repository";
 
 export class ItemUseCase {
@@ -7,34 +7,14 @@ export class ItemUseCase {
 		this.itemRepository = itemRepository;
 	}
 	//
-	//Get all itemss
-	async getItemsList() {
-		return this.itemRepository.getAllItems();
-	}
-	//
-	//	// get the items involved in manufacturing and use them to construct an object of all the necessary details. For example, item name, quantity, uom, cost price etc. Include even the raw materials needed in the manufacturing plants.
-	async compileItemListData({
+	//Get all items
+	async getItemsList({
 		limit_page_length,
-	}: Omit<ItemListFieldsEntity, "fields">): Promise<AssociatedItemsEntity[]> {
-		const salesItems = await this.itemRepository.listAllSalesItems({
+		limit_start,
+	}: ManufacturingMaterialsFieldsEntity) {
+		return this.itemRepository.getAllItems({
 			limit_page_length,
-		});
-		const allItems = await this.itemRepository.getAllItems();
-
-		return salesItems.map(salesItems => {
-			const matchedItems = allItems.filter(item =>
-				salesItems.item_name.includes(item.name)
-			);
-			if (matchedItems.length > 0) {
-				return {
-					...salesItems,
-					associated_items: matchedItems,
-				};
-			}
-			return {
-				...salesItems,
-				associated_items: [],
-			};
+			limit_start,
 		});
 	}
 }
