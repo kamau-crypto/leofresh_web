458071eb35caf9ca3a81ecef21d7325083db6f81
Peter Kamau <kamaupeter343@gmail.com>
2025-09-16 23:02:14 +0300

    Added create BOM Entity

--- src/domain/entities/bom/bom.entity.ts
+++ src/domain/entities/bom/bom.entity.ts
@@ -15,4 +15,23 @@ export interface GetBOMsFilterEntity extends CommonEntity {
 	default_target_warehouse?: string;
 }
 
-export interface CreateBOMEntity {}
+export interface CreateBOMEntity {
+	item: string;
+	quantity: number;
+	is_active: number;
+	is_default: number;
+	rm_cost_as_per: string;
+	default_target_warehouse?: string;
+	default_source_warehouse?: string;
+	uom: string;
+	items: CreateBOMItemEntity[];
+	process_loss_percentage: number;
+	process_loss_qty: number;
+}
+
+export interface CreateBOMItemEntity {
+	item_code: string;
+	uom: string;
+	qty: number;
+	rate: number;
+}
