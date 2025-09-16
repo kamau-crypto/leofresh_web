06fe4f1fa413132a08d55642b11ac0f69bbcc5dc
Peter Kamau <kamaupeter343@gmail.com>
2025-09-17 00:08:19 +0300

    Added the query creation hook

--- src/hooks/bom.ts
+++ src/hooks/bom.ts
@@ -1,11 +1,15 @@
 import { useAuth } from "@/components";
-import type { GetBOMsFilterEntity } from "@/domain";
+import type { CreateBOMEntity, GetBOMsFilterEntity } from "@/domain";
 import { LeofreshError } from "@/lib/error";
 import { BOMRepository } from "@/repository";
 import { BOMUseCase } from "@/use-cases/bom.use-case";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-export function useListBOMs({ params }: { params: Omit<GetBOMsFilterEntity, "fields"> }) {
+export function useListBOMs({
+	params,
+}: {
+	params: Omit<GetBOMsFilterEntity, "fields">;
+}) {
 	const { user } = useAuth();
 	const { data, isLoading, error } = useQuery({
 		queryKey: ["boms-list", user?.user?.email, params],
@@ -24,7 +28,26 @@ export function useListBOMs({ params }: { params: Omit<GetBOMsFilterEntity, "fie
 	return { isLoading, data };
 }
 
-
 export function useCreateBOM() {
-    
+	const { user } = useAuth();
+	const queryClient = useQueryClient();
+	const { mutateAsync, error, data } = useMutation({
+		mutationKey: ["create-bom", user?.user?.email],
+		mutationFn: async (data: CreateBOMEntity) => {
+			if (!user?.user?.email)
+				throw new LeofreshError({ message: "User not authenticated" });
+			const bomRepo = new BOMRepository();
+			const bomUseCase = new BOMUseCase({ bomRepository: bomRepo });
+			return await bomUseCase.createBOMs(data);
+		},
+		onSettled: () => {
+			queryClient.invalidateQueries({ queryKey: ["bom-list"] });
+		},
+	});
+
+	if (error instanceof Error) {
+		throw new LeofreshError({ message: error.message });
+	}
+
+	return { mutateAsync, data };
 }
