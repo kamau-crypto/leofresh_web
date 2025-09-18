0d9dc789da0dd29d77c3e18071a9d123c388d89d
Peter Kamau <kamaupeter343@gmail.com>
2025-09-18 10:55:29 +0300

    Added the work order page components

--- src/components/pages/work-order/workorder.tsx
+++ src/components/pages/work-order/workorder.tsx
@@ -1,2 +1,34 @@
-//
-//Pull the list of all work orders at this stage....
+import {
+	LeoButton,
+	LeoFreshDialogForm,
+	LeoWrapper,
+} from "@/components/leofresh";
+import { useAppSelector } from "@/hooks/appHooks";
+import { PlusCircle } from "lucide-react";
+import { WorkOrderForm } from "./form/WorkOrderForm";
+import { WorkOrderList } from "./list/WorkOrderList";
+
+export function WorkOrder() {
+	const { profile } = useAppSelector(state => state.profile);
+
+	return (
+		<LeoWrapper
+			description='Manage and track manufacturing work orders within your warehouse.'
+			title='Work Orders'
+			Content={<WorkOrderList />}
+			HeaderButtons={
+				<LeoFreshDialogForm
+					description={`Start a manufacturing process in the warehouse ${profile!.warehouse_name}`}
+					title='New Work Order'
+					DialogTriggerChild={
+						<LeoButton>
+							<PlusCircle className='siz-4 ml-2' />
+							Create Work Order
+						</LeoButton>
+					}
+					DialogChild={<WorkOrderForm />}
+				/>
+			}
+		/>
+	);
+}
