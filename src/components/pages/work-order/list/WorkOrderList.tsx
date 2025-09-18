169c579abf3fe9acfe4b6723fb8d77cfddf08a84
Peter Kamau <kamaupeter343@gmail.com>
2025-09-18 10:56:20 +0300

    Added the work order list for reuse within the app

--- src/components/pages/work-order/list/WorkOrderList.tsx
+++ src/components/pages/work-order/list/WorkOrderList.tsx
@@ -1,1 +1,44 @@
+import { LeofreshDataTable } from "@/components/leofresh";
+import type { ListWorkOrderFilterEntity } from "@/domain";
+import { useAppSelector } from "@/hooks/appHooks";
 
+import { useListWorkOrders } from "@/hooks/work.order";
+import { useEffect, useMemo, useState } from "react";
+import { woColumns } from "./columns";
+
+export function WorkOrderList() {
+	const { profile } = useAppSelector(state => state.profile);
+
+	const [filter, setFilter] = useState<
+		Omit<ListWorkOrderFilterEntity, "fields">
+	>({
+		limit_page_length: 100,
+		limit_start: 0,
+		order_by: "creation desc",
+		filters: [["fg_warehouse", "=", profile!.warehouse_name]],
+	});
+
+	const { data, isLoading } = useListWorkOrders({ ...filter });
+
+	useEffect(() => {
+		setFilter(prev => ({
+			...prev,
+			filters: [["fg_warehouse", "=", profile!.warehouse_name]],
+		}));
+	}, [profile?.warehouse_name]);
+
+	const memoizedData = useMemo(() => {
+		if (!data) return [];
+		return data;
+	}, [data]);
+
+	return (
+		<LeofreshDataTable
+			columns={woColumns}
+			data={memoizedData}
+			isLoading={isLoading}
+			filterPlaceHolder={"Work Order Name"}
+			primaryFilter={"name"}
+		/>
+	);
+}
